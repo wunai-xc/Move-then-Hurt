ee5d7ba18e3d1aa354a903af5194c6ffa6777b26
wunai-xc <3234319738@qq.com>
2026-05-10 03:05:11 +0800

    更新 Unit.java

--- src/main/java/com/example/cardgame/model/Unit.java
+++ src/main/java/com/example/cardgame/model/Unit.java
@@ -7,8 +7,49 @@ public class Unit {
     private boolean isKing;
 
     public Unit(Card card, Player owner, boolean isKing) {
-        this.card = card; this.owner = owner; this.isKing = isKing;
+        this.card = card;
+        this.owner = owner;
+        this.isKing = isKing;
         this.currentHp = card.getHp();
     }
-    // getter/setter...
+
+    // Getter 和 Setter
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
+    }
+
+    public void setCurrentHp(int currentHp) {
+        this.currentHp = currentHp;
+    }
+
+    public boolean isKing() {
+        return isKing;
+    }
+
+    public void setKing(boolean king) {
+        isKing = king;
+    }
+
+    // 深拷贝（可选，供 GameState.deepCopy 使用）
+    public Unit deepCopy() {
+        Unit copy = new Unit(this.card, this.owner, this.isKing);
+        copy.currentHp = this.currentHp;
+        return copy;
+    }
 }
